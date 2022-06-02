6a71421cdd5d149e39f45be0022f0abdd5dc9d60
marktiwnzhao <3239168744@qq.com>
2022-06-02 23:20:57 +0800

    zhao edited

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import jieba
-import numpy as np
-import pandas as pd
-import word2vec
+# import jieba
+# import numpy as np
+# import pandas as pd
+# import word2vec
 
 if __name__ == '__main__':
+    print("Hello world")
